479f862e16ff08184dac5c9d373234201cc384ed
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-09 17:10:19 +0700

    Commit

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(MainActivity.this,
+                com.example.twoactivities.SecondActivity.class);
+        startActivity(intent);
+    }
+}
