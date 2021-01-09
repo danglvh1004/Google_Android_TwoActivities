aa255463f17a46b18890b4280337e94927873028
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-09 20:43:49 +0700

    Commit

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
+
         Intent intent = new Intent(MainActivity.this,
                 com.example.twoactivities.SecondActivity.class);
+
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra("message_send", message);
         startActivity(intent);
     }
 }
